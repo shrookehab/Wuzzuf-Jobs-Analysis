0034be6efecfea817e5e0831d51067e1774c74a5
Abdullah-Essam <Abdullah-Essam@users.noreply.github.com>
2022-03-16 13:33:16 +0200

    Initial commit

---
+++ src/main/java/com/team3/WuzzafJobs/WuzzafJobsApplication.java
@@ -0,0 +1,13 @@
+package com.team3.WuzzafJobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WuzzafJobsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WuzzafJobsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team3/WuzzafJobs/WuzzafJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team3.WuzzafJobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WuzzafJobsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
